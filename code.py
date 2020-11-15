5fd9c000d605db4ee1dea5958fa16ed2448d045d
kenny <kenny@sourcetonuts.com>
2020-11-15 08:12:26 -0500

    optimization strategy in attempt to remove visual steps

--- code.py
+++ code.py
@@ -1,95 +1,103 @@
 import time
 import board
-import microcontroller
-from digitalio import DigitalInOut, Direction, Pull
-import adafruit_dotstar as dotstar
+import touchio
+import neopixel
 import adafruit_fancyled.adafruit_fancyled as fancy
 
-# with the following 96 LED setup on an Adafruit Trinket M0
-# it could be wired up to run off of a basic/standard USB charger or laptop
-# cabling required for Trinket
-#   USB micro for power
-#   pin 1 to touch button/copper pad
-#   pin 3 to Dotstar clock input pin
-#   pin 4 to Dotstar data input pin
-#   along with USB and GND from the Trinket for NeoPixel power/ground/data-in 3 conductor cable
+num_pixels = 30
 
-pin_touch = board.A0
-pin_status = board.D13
+# pin usage: TRINKET: board.D4, GEMMA: board.D1
+strip = neopixel.NeoPixel( board.D1, num_pixels, brightness = 0.05, auto_write = False )
+#, pixel_order= neopixel.RGB
 
-num_pixels = 54
-strip = dotstar.DotStar( board.SCK, board.MOSI, num_pixels, brightness=1.0, auto_write=False )
-print( "BouncingComet" )
+print( "Steelyard #1 Gemma M0" )
 
-# refer to for gradiant / color info
-# https://learn.adafruit.com/fancyled-library-for-circuitpython/led-colors
+# TouchMode
+# this class manages a single captouch pin and cycles through modes
+# on that pin up to provided lastmode
+#
+class TouchMode :
+    def __init__( self, inputMode, num = 2, name = None ) :
+        self.value = 0
+        self.lastmode = num - 1
+        self.input = inputMode
+        self.name = name or "mode"
 
+    def update( self ) :
+        wasoff = not self.input.value
+        time.sleep(0.005)  # 5ms delay for debounce
+        if not wasoff and self.input.value :
+            # just touched button
+            time.sleep(0.005)  # 5ms delay for debounce
+            if self.value >= self.lastmode :
+                self.value = 0
+            else :
+                self.value = self.value + 1
+            # debounce
+            time.sleep( 0.5 )
+            print( self.name + " {}".format( self.value ) )
+        return self.value
 
-start = 0
-length = 5
-BLACK = (0,0,0)
+class RainMan :
+    def __init__( self, strip ) :
+        # refer to
+        # https://learn.adafruit.com/fancyled-library-for-circuitpython/led-colors
+        # across the rainbow
+        self.strip = strip
+        self.lookup = []
+        self.size = strip.n * 2
 
-hue = 0.0
-sat = 1.0
-bri = 0.4
+        grad = [ (0.0,0xFF0000), (0.33,0x00FF00), (0.67,0x0000FF), (1.0,0xFF0000)]
+        palette = fancy.expand_gradient( grad, 20 )
+        for index in range(self.size) :
+            coloff = index / self.size
+            rgb = fancy.palette_lookup( palette, coloff )
+            color = rgb.pack()
+            self.lookup.append( color )
 
-delay = 0.002
-satdelta = 0.08
+    def color_selected( self, coloff ) :
+        coloff = coloff % 1
+        colindex = int( coloff * self.size )
+        return self.lookup[colindex]
 
-strip.fill(BLACK);
-strip.show();
+    def show_static( self, coloff ) :
+        color = self.color_selected( coloff )
+        strip.fill( color )
+        strip.show()
 
-def draw_comet_up() :
-    next = start
-    end = start + length
-    nextsat = sat - ( ( length - 1 ) * satdelta )
+    def palette_cycle( self, coloff ) :
+        for index in range( num_pixels ) :
+            offset = coloff + ( index / num_pixels )
+            rgb = self.color_selected( offset % 1 )
+            strip[index] = rgb
+        strip.show()
 
-    if ( next >= 0 ) :
-        strip[next] = BLACK
+display = RainMan( strip )
 
-    while ( next < end ) :
-        nextsat = nextsat + satdelta
-        color = fancy.CHSV(hue,nextsat,bri)
-        next = next + 1
-        strip[next] = color.pack()
+# OnOff pin usage: TRINKET: board.A0, GEMMA: board.A1
+# Mode pin usage: TRINKET: board. TBD ??, GEMMA: board.A2
 
-    strip.show()
+inputOnOff = touchio.TouchIn( board.A1 )
+onoffMachine = TouchMode( inputOnOff, 2, "onoff" )
+inputMode = touchio.TouchIn( board.A2 )
+modeMachine = TouchMode( inputMode, 3 )
 
-
-def draw_comet_down() :
-    next = start
-    end = start + length
-    nextsat = sat
-
-    while ( next < end ) :
-        color = fancy.CHSV(hue,nextsat,bri)
-        next = next + 1
-        strip[next] = color.pack()
-        nextsat = nextsat - satdelta
-
-    next = next + 1
-    if ( next <= last_index ) :
-        strip[next] = BLACK
-    strip.show()
-
-
-up = True
-last_index = num_pixels - 1
+offset = 0.001
+mode = 0
 
 # Loop Forever
 while True :
-    time.sleep( delay )
-
-    if up :
-        draw_comet_up()
-        start = start + 1
-        delay = delay + 0.004
+    onoff = onoffMachine.update()
+    if onoff == 0 :
+        display.palette_cycle( offset )
+        offset += 0.005 # 0.035 # this sets how quickly the rainbow changes (bigger is faster)
     else :
-        draw_comet_down()
-        start = start - 1
-        delay = delay - 0.004
+        newmode = modeMachine.update()
+        if mode != newmode :
+            mode = newmode
 
-    if ( ( start + length ) >= last_index ) :
-        up = False
-    elif ( start == 0 ) :
-        up = True
+        if mode == 0 :
+            # and if just off just off paint/fill w/ the center color
+            display.show_static( offset )
+        else :
+            display.show_static( offset )# Write your code here :-)
